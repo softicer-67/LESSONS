252430542198c94ab61d441431a81fd96c58930a
Yuri_1967 <9022368@mail.ru>
2021-07-21 18:44:07 +0500

    Задача 5

--- ALGORITMS/Lesson_3/5.py
+++ ALGORITMS/Lesson_3/5.py
@@ -4,11 +4,11 @@
 
 import random
 
-arr = [random.randint(-100, 100) for i in range(10)]
+arr = [random.randint(-100, 100) for _ in range(10)]
 print('\n', arr)
 print('\n Максимальный отрицательный элемент:', min(arr))
 
-for i in range(len(arr)):
+for _ in range(len(arr)):
     mini = min(arr)
     ind = arr.index(mini)
 print(' Находится на позиции:', ind + 1)
