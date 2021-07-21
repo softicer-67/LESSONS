33a6070e946a24bbe8d9f78c453b02c6d2cd35a6
Yuri_1967 <9022368@mail.ru>
2021-07-22 01:01:09 +0500

    Задача 7

--- ALGORITMS/Lesson_3/7.py
+++ ALGORITMS/Lesson_3/7.py
@@ -3,18 +3,21 @@
 Они могут быть как равны между собой (оба являться минимальными), так и различаться.
 '''
 
+
 import random
 
 arr = [random.randint(1, 99) for _ in range(11)]
 print('\n', arr)
 
+sort_arr = sorted(arr)
+print(' Два наименьших элемента в массиве', sort_arr[0], 'и', sort_arr[1], '- легкий вариант через  сортировку')
+
+
 mini_1 = min(arr)
 min_ind = arr.index(mini_1)
-
 del arr[min_ind]
-
 mini_2 = min(arr)
-print(' Два наименьших элемента в массиве', mini_1, 'и', mini_2)
+print(' Два наименьших элемента в массиве', mini_1, 'и', mini_2, '- совершенно бредовый вариант, но тоже сработало ;)')
 
 
 
