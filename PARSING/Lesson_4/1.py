a6809cd18710b8dff82898041c6cdc7febd88493
Yuri_1967 <9022368@mail.ru>
2021-08-11 14:50:32 +0500

    Задача 1

--- PARSING/Lesson_4/1.py
+++ PARSING/Lesson_4/1.py
@@ -14,9 +14,9 @@ import requests
 header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
 
 
-def request_to_mail(_str):
+def request_to_mail():
     try:
-        response = requests.get('https://news.mail.ru/', params={'text': _str}, headers=header)
+        response = requests.get('https://news.mail.ru/', headers=header)
         root = html.fromstring(response.text)
         for i in range(1, 5):
             news = root.xpath(f'//*[@id="index_page"]/div[7]/div[2]/div[3]/div/div/div/div[{i}]/div/div[2]/span[2]/a/span')
@@ -28,4 +28,4 @@ def request_to_mail(_str):
         print('Ошибка запроса')
 
 
-request_to_mail('')
+request_to_mail()
