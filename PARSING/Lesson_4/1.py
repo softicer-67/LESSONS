bfb3396d2561f2f3e7997acb87c2a0f13828443b
Yuri_1967 <9022368@mail.ru>
2021-08-11 14:42:11 +0500

    Задача 1

--- PARSING/Lesson_4/1.py
+++ PARSING/Lesson_4/1.py
@@ -19,11 +19,11 @@ def request_to_mail(_str):
         response = requests.get('https://news.mail.ru/', params={'text': _str}, headers=header)
         root = html.fromstring(response.text)
         for i in range(1, 5):
-            result = root.xpath(f'//*[@id="index_page"]/div[7]/div[2]/div[3]/div/div/div/div[{i}]/div/div[2]/span[2]/a/span')
+            news = root.xpath(f'//*[@id="index_page"]/div[7]/div[2]/div[3]/div/div/div/div[{i}]/div/div[2]/span[2]/a/span')
             link = root.xpath(f'//*[@id="index_page"]/div[7]/div[2]/div[3]/div/div/div/div[{i}]/div/div[2]/span[2]/a/@href')
             ist = root.xpath(f'//*[@id="index_page"]/div[7]/div[2]/div[3]/div/div/div/div[{i}]/div/div[2]/div/span[2]/text()')
             tim = root.xpath(f'//*[@id="index_page"]/div[7]/div[2]/div[3]/div/div/div/div[{i}]/div/div[2]/div/span[1]')
-            print([ist[0]], result[0].text, ' ', link[0], tim[0].text)
+            print([ist[0]], news[0].text, ' ', link[0], tim[0].text)
     except:
         print('Ошибка запроса')
 
