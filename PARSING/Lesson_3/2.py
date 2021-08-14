30bd9284da0d3ac4932e414b9f58448eff8a36bf
Yuri <57116431+softicer-67@users.noreply.github.com>
2021-08-14 20:21:11 +0500

    Update 2.py

--- PARSING/Lesson_3/2.py
+++ PARSING/Lesson_3/2.py
@@ -6,26 +6,21 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
+
 header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
 
 
 def get_data(zp):
     resp = requests.get(f'https://www.rabota.ru/?sort=relevance&min_salary={zp}', headers=header)
     soup = bs(resp.text, 'lxml')
-    work = []
-    price = []
     result = []
+    work = soup.find_all(class_="vacancy-preview-card__title")
+    price = soup.find_all(class_="vacancy-preview-card__salary vacancy-preview-card__salary-blue")
     for i in range(20):
-        w = soup.find_all(class_="vacancy-preview-card__title")
-        work.append(w[i].text.strip())
-        p = soup.find_all(class_="vacancy-preview-card__salary vacancy-preview-card__salary-blue")
-        price.append(p[i].text.strip().replace('\xa0', ' '))
-
         result.append({
-            'Вакансия': work[i],
-            'Зарплата': price[i]
+            'Вакансия': work[i].text.strip(),
+            'Зарплата': price[i].text.strip().replace('\xa0', ' ')
         })
-
         print(result[i])
 
 
