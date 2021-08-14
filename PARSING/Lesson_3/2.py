c4d72f592c11ef865709b51aa01432549244e3b8
Yuri <57116431+softicer-67@users.noreply.github.com>
2021-08-14 23:11:07 +0500

    Update 2.py

--- PARSING/Lesson_3/2.py
+++ PARSING/Lesson_3/2.py
@@ -5,9 +5,11 @@
 
 import requests
 from bs4 import BeautifulSoup as bs
+from pprint import pprint
 
 
 header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
+url = 'https://www.rabota.ru'
 
 
 def get_data(zp):
@@ -15,13 +17,16 @@ def get_data(zp):
     soup = bs(resp.text, 'lxml')
     result = []
     work = soup.find_all(class_="vacancy-preview-card__title")
+    link = soup.find_all('h3', class_='vacancy-preview-card__title')
     price = soup.find_all(class_="vacancy-preview-card__salary vacancy-preview-card__salary-blue")
     for i in range(20):
         result.append({
             'Вакансия': work[i].text.strip(),
-            'Зарплата': price[i].text.strip().replace('\xa0', ' ')
+            'Зарплата': price[i].text.strip().replace('\xa0', ' '),
+            'Ссылка': url + link[i].find('a').get('href')
         })
-        print(result[i])
+        pprint(result[i])
+        print('-' * 90)
 
 
 get_data(80000)  # Требуемая зарплата
